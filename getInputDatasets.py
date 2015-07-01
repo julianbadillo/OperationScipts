6e66bcdfeb184a86551038525fee6f6cab5487eb
Julian Badillo <jbadillo@cern.ch>
2015-07-01 16:20:58 +0200

    file options.

--- getInputDatasets.py
+++ getInputDatasets.py
@@ -3,15 +3,28 @@
 # Needs WmAgentScripts libraries
 
 import WmAgentScripts.reqMgrClient as reqMgr
-import sys
+import sys, optparse
 
 def main():
-    f = sys.argv[1]
+    usage = 'python %prog [OPTIONS] [WORKFLOW]'
+    parser = optparse.OptionParser(usage = usage)
+    parser.add_option('-f', '--file', help='Text file with several workflows',dest='file')
+    (options, args) = parser.parse_args()
+
     url = 'cmsweb.cern.ch'
-    for wf in open(f):
+     
+    if options.file:
+        wfs = [l.strip() for l in open(options.file) if l.strip()]
+    else:
+        wfs = [args[0]]
+    #Several wfs in one go
+    for wf in wfs:
         ds = reqMgr.getInputDataSet(url, wf)
+        print wf
         print ds
 
 if __name__ == '__main__':
     main()
+
+
 
